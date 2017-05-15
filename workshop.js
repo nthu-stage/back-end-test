aa95c486902fc7fad9c05006d535573dee516f67
Steven <stevenlin0918@gmail.com>
2017-05-15 23:02:35 +0800

    [Update]Add workshop content

---
+++ workshop.js
@@ -0,0 +1,46 @@
+module.exports = [
+    {
+        img_url : "https://www.britishcouncil.org.tw/sites/default/files/styles/bc-landscape-630x354/public/le-tc-adult-general-1.jpg?itok=FSdEEOFo",
+        location:'台北101',
+        content:`如果你的格局只是為了競爭，那你的文案就只能寫那些促銷殺價文案。
+
+把眼光放在客人身上，你的文案才能寫出他們要的價值。
+
+假設今天你老是覺得你們公司的文案寫得不是那麼好。
+
+那請先思考，你的品牌給他們的感覺，是不是格局也很不好呢。
+
+或許我們都無法真的做出什麼驚世駭俗的超級產品。
+
+最起碼，我們的境界，可以不要只是為了比較。
+
+品牌的競爭，永遠不是跟別人比。
+
+我們拚命進步，是因為不想辜負。`,
+        title: '文案的100種寫法',
+        start_datetime: '2017-11-11 18:00',
+        end_datetime: '2017-11-11 21:00',
+        min_number: '100',
+        max_number: '50',
+        deadline: '2017-11-5',
+        introduction: '文案的價值，體現在它的內容',
+        price: '1500',
+        phase:'reached',
+        name: '文卿大師兄'
+    },
+    {
+        img_url : "https://s3.amazonaws.com/huarenke-bucket/wp-content/uploads/2016/08/09173025/py8.jpg",
+        location:'彰化八卦山',
+        content:'有些人剛入門的時候就會買最頂尖最昂貴的攝影器材，但有時候不一定要用最昂貴的器材才能拍到好照片的。在不久之前才有一名 90 後攝影師用一部iPhone 拍出奪得 National Geographic 攝影比賽獎項，證明器材的平貴跟相片的好壞沒有一定關係，器材不是最重要的，一雙「攝影眼」才是拍出好相片的關鍵。',
+        title: '10 個給攝影初學者的有用建議',
+        start_datetime: '2017-08-11 08:00',
+        end_datetime: '2017-08-12 21:00',
+        min_number: '40',
+        max_number: '20',
+        deadline: '2017-08-01',
+        introduction: '對於攝影初學者，有沒有甚麼小貼士可以讓技巧更進步，或者在學習階段有甚麼要留意呢？這裏有10個要點，供各位參考一下吧！',
+        price: '100',
+        phase:'investigating',
+        name:'化身單眼的男人'
+    }
+];
